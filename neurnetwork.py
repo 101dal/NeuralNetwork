cf715aade59499ba79f6bc5484d39324f7edd274
Augustin Michez <augustin.michezgrenier@gmail.com>
2023-03-09 22:08:23 +0100

    Test

--- neurnetwork.py
+++ neurnetwork.py
@@ -1,5 +1,10 @@
-def neuron_network(input_layer,hidden_layer,output_layer):
-    input_layer = list(input_layer)
+def neuron_network(hidden_layer):
     hidden_layer = list(hidden_layer)
-    output_layer = list(output_layer)
-    return str(input_layer) + str(hidden_layer) + str(output_layer)
+    lo = []
+    for i in range(len(hidden_layer)-1):
+        lo += hidden_layer[i]
+    return lo
+
+input = [0,0,0,0]
+a = neuron_network([[input],[input],[0]])
+print(a)
