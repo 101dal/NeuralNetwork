8b635eec6fb5df94f5c018c54d9c8530678ceddf
Augustin Michez <augustin.michezgrenier@gmail.com>
2023-03-09 22:05:08 +0100

    Main

---
+++ neurnetwork.py
@@ -0,0 +1,5 @@
+def neuron_network(input_layer,hidden_layer,output_layer):
+    input_layer = list(input_layer)
+    hidden_layer = list(hidden_layer)
+    output_layer = list(output_layer)
+    return str(input_layer) + str(hidden_layer) + str(output_layer)
